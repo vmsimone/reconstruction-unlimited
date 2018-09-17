444369007d4c19e50a5710c683cb3fede854f9df
Vincent Simone <vsimone@protonmail.com>
2018-09-16 23:28:30 -0700

    cosmetic updates

--- recon.js
+++ recon.js
@@ -1,12 +1,13 @@
 function readyTestimonialsLink() {
-  
+  //optional
+  //replace with function that can take out the ".html" at the end of the page name
 }
 
 function readyScroll() {
   $('#fullpage').fullpage({
     autoScrolling: true
   });
-  readyTestimonialsLink();
+  //readyTestimonialsLink();
 }
 
 $(readyScroll);
